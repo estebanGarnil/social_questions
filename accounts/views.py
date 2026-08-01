9e2bf0e0cd7bfbfca7971bf13103d82541d686b2
estebanGarnil <esteb.garnil@gmail.com>
2026-08-01 17:30:04 +0200

    Harden Nginx security headers

--- accounts/views.py
+++ accounts/views.py
@@ -8,10 +8,10 @@ from drf_spectacular.utils import (
 from rest_framework import generics, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.throttling import ScopedRateThrottle
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.throttling import ScopedRateThrottle
 
 from question_lists.serializers import QuestionListSerializer
 
